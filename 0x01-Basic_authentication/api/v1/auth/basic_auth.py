7cf81bd39c173ff45b018c2d846e54d00dc1cbb2
Darius Daniel <dariusdaniel771@gmail.com>
2023-11-10 04:14:56 +0100

    Created a method in BasicAuth to return the decode value of a Base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Implements a class that inherits from Auth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,18 @@ class BasicAuth(Auth):
                 not authorization_header.startswith('Basic ')):
             return None
         return authorization_header.split('Basic ')[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Return the decoded value of a Base64 string
+        base64_authorization_header
+        """
+        if (
+                base64_authorization_header is None or
+                not isinstance(base64_authorization_header, str)):
+            try:
+                decoded_header = base64.b64decode(base64_authorization_header)
+            except Exception:
+                return None
+            else:
+                return decoded_header.decode('utf-8')
