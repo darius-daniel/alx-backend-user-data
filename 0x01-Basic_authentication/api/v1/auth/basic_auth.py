16930326778cbcdf5e789f43b18f9b3e5f321048
Darius Daniel <dariusdaniel771@gmail.com>
2023-11-10 04:07:20 +0100

    Created a method in BasicAuth to return the Base64 part of an Authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,14 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ BasicAuth implementation.
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ Return Base64 part of the Authorization header for a Basic
+        Authentication
+        """
+        if (
+                authorization_header is None or
+                not isinstance(authorization_header, str) or
+                not authorization_header.startswith('Basic ')):
+            return None
+        return authorization_header.split('Basic ')[1]
