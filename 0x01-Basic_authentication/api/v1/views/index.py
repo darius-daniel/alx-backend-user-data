d3e508fbf02031f9dc4452b008ccdb1ed8d1f898
Darius Daniel <dariusdaniel771@gmail.com>
2023-11-10 02:00:46 +0100

    Added correct return type annotation to endpoint handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,7 +27,7 @@ def stats() -> str:
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized():
+def unauthorized() -> None:
     """ Raises an error
     """
     abort(401)
