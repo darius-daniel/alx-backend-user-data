bbdec518ae9c7c1c8396b0339e62028c511ae41c
Darius Daniel <dariusdaniel771@gmail.com>
2023-11-10 01:55:49 +0100

    removed return type annotation from endpoint

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,7 +27,7 @@ def stats() -> str:
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
+def unauthorized():
     """ Raises an error
     """
     abort(401)
