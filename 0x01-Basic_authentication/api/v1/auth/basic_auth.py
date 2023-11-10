25f194b177113bf4ff2b07d647e4cad6125547ae
Darius Daniel <dariusdaniel771@gmail.com>
2023-11-10 03:27:55 +0100

    Created an empty class that inherits from Auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Implements a class that inherits from Auth
+"""
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """ BasicAuth implementation.
+    """
+    pass
