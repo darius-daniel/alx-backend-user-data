7e4bfc3c2518594f3457b6c59fe3fa41e9c06853
Darius Daniel <dariusdaniel771@gmail.com>
2023-11-10 05:01:44 +0100

    Modified conditional

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -46,7 +46,7 @@ class BasicAuth(Auth):
         if (
                 decoded_base64_authorization_header is None or
                 not isinstance(decoded_base64_authorization_header, str) or
-                ':' not in decoded_base64_authorization_header):
+                not ':' in decoded_base64_authorization_header):
             return None, None
         return tuple(decoded_base64_authorization_header.split(':'))
 
