89789103964a005f1f604b31f028555078b4168f
Darius Daniel <dariusdaniel771@gmail.com>
2023-11-11 03:17:10 +0100

    Converted generated session_id to string before assigning to variable

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -14,7 +14,7 @@ class SessionAuth(Auth):
         """ Creates a Session ID for a user_id.
         """
         if user_id and isinstance(user_id, str):
-            session_id = uuid.uuid4()
+            session_id = str(uuid.uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
 
