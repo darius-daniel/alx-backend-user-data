ea9570e6795c7db6a26c107971adaab82e69051d
Darius Daniel <dariusdaniel771@gmail.com>
2023-11-11 03:07:46 +0100

    Fixed pycodstyle errors

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,12 +9,13 @@ class SessionAuth(Auth):
     """ SessionAuth inherits from Auth.
     """
     user_id_by_session_id = {}
+
     def create_session(self, user_id: str = None) -> str:
         """ Creates a Session ID for a user_id.
         """
         if user_id and isinstance(user_id, str):
-           session_id = uuid.uuid4()
-           self.user_id_by_session_id[session_id] = user_id
-           return session_id
+            session_id = uuid.uuid4()
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
 
         return None
