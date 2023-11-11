dc4ba9226eb29e4d440e4c612bf2b508813b1eb1
Darius Daniel <dariusdaniel771@gmail.com>
2023-11-11 03:04:13 +0100

    Created class attribute user_id_by_session_id  and instance method create_session in class SessionAuth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,19 @@
 """ Implements a class SessionAuth that inherits from Auth.
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ SessionAuth inherits from Auth.
     """
-    pass
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a Session ID for a user_id.
+        """
+        if user_id and isinstance(user_id, str):
+           session_id = uuid.uuid4()
+           self.user_id_by_session_id[session_id] = user_id
+           return session_id
+
+        return None
